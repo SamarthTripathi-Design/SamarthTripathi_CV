cf5e384321a38b4cf6cb4b1c9d7a7fd9b6432ef5
Samarth work <tripathi.samarth@gmail.com>
2021-07-27 09:57:51 +0530

    final commit

--- main.js
+++ main.js
@@ -48,24 +48,44 @@ function scrollActive() {
 window.addEventListener("scroll", scrollActive);
 
 /*==================== SHOW SCROLL TOP ====================*/
-
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 200) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollTop);
 /*==================== DARK LIGHT THEME ====================*/
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
-
+function scaleCv() {
+  document.body.classList.add("scale-cv");
+}
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
-
+function removeScale() {
+  document.body.classList.remove("scale-cv");
+}
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
-
+let areaCv = document.getElementById("area-cv");
+let resumeButton = document.getElementById("resume-button");
 // Html2pdf options
-
+let opt = {
+  margin: 0,
+  filename: "myResume.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 2 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
 // Function to call areaCv and Html2Pdf options
-
+function generateResume() {
+  html2pdf(areaCv, opt);
+}
 // When the button is clicked, it executes the three functions
-
-// 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
-// 2. The PDF is generated
-
-// 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+resumeButton.addEventListener("click", () => {
+  // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+  scaleCv();
+  // 2. The PDF is generated
+  generateResume();
+  // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+});
